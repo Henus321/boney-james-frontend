5400a9888f3508d7e45d0aa769ccfdac06f238f0
Alexander Erkhov <tyrantbud@yandex.ru>
2024-08-11 18:28:49 +0300

    feat/layout: Add header animation on scroll. Fix layout style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,67 @@
 import { Link } from "react-router-dom";
 import { RoutesStructure } from "../../config";
 import { FaHeart, FaShoppingBag, FaUser } from "react-icons/fa";
-//import logo from "../../assets/logo.png";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
 
-import "./index.scss";
 import Typo from "../Typo";
 
+import "./index.scss";
+
 const Header = () => {
-    // TODO listener on top 0, to show full logo => just name with fade animation
+    const [headerType, setHeaderType] = useState<"full" | "small">("full");
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll, { passive: true });
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        setHeaderType(window.scrollY !== 0 ? "small" : "full");
+    };
 
     return (
-        <header className="header">
+        <header
+            className={classNames(
+                "header",
+                headerType === "small" && "header--small"
+            )}
+        >
             <nav className="header__nav">
                 <ul>
                     <li>
-                        <Link to="/">Главная</Link>
+                        <Link to={RoutesStructure.Index}>Главная</Link>
                     </li>
                     <li>
+                        {/* TODO route/page */}
                         <Link to="/">Магазины</Link>
                     </li>
                     <li>
+                        {/* TODO route/page */}
                         <Link to="/">Покупателям</Link>
                     </li>
                 </ul>
             </nav>
 
-            <Link className="header__logo" to={RoutesStructure.Index}>
-                {/*<img src={logo} alt="logo" />*/}
+            <Link
+                className={classNames(
+                    "header__logo",
+                    headerType === "small" && "header__logo--small"
+                )}
+                to={RoutesStructure.Index}
+            >
                 <Typo type="h1">Boney james</Typo>
             </Link>
 
-            <div className="header__menu">
+            <div
+                className={classNames(
+                    "header__menu",
+                    headerType === "small" && "header__menu--small"
+                )}
+            >
                 <FaHeart onClick={() => console.log("Click bookmark")} />
                 <FaShoppingBag onClick={() => console.log("Click cart")} />
                 <FaUser onClick={() => console.log("Click profile")} />
