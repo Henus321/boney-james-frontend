4996e19df2b2b6e27cecc83c5e4600ed971e6015
Alexander Erkhov <tyrantbud@yandex.ru>
2024-08-11 14:31:41 +0300

    feat/error-handle: Add toastify notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RoutesStructure } from "./config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 
 import ErrorBoundary from "./components/ErrorBoundary";
 import Layout from "./components/Layout";
 import IndexPage from "./pages/IndexPage";
 import CoatPage from "./pages/CoatPage";
 import NotFoundPage from "./pages/NotFoundPage";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./styles/globals.scss";
 
 const queryClient = new QueryClient();
 
@@ -29,6 +33,7 @@ const App = () => {
                             <Route path="*" element={<NotFoundPage />} />
                         </Route>
                     </Routes>
+                    <ToastContainer position="bottom-right" />
                 </ErrorBoundary>
             </BrowserRouter>
         </QueryClientProvider>
